fa8b289a3d2f99ebcfd58670331f09c7fad3da52
DanilHN <95386266+DanilHN@users.noreply.github.com>
2022-01-10 08:33:35 +0200

    firstcommit

---
+++ src/components/Controls/Controls.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import setTimeFormat from '../../utils/helpers';
+
+export const Controls = ({
+  time,
+  start,
+  stop,
+  reset,
+  wait,
+}) => (
+  <>
+    <header className="header">
+      <h1 className="stopwatch caption">
+        Stopwatch
+      </h1>
+      <h1 className="stopwatch clock">
+        {setTimeFormat(time)}
+      </h1>
+    </header>
+    <section className="main">
+      <div className="case">
+        <button type="button" className="button b_style" onClick={start}>
+          Start
+        </button>
+        <button type="button" className="button b_style" onClick={stop}>
+          Stop
+        </button>
+        <button type="button" className="button b_style" onClick={reset}>
+          Reset
+        </button>
+        <button type="button" className="button b_style" onClick={wait}>
+          Wait
+        </button>
+      </div>
+    </section>
+  </>
+);
+
+Controls.propTypes = {
+  time: PropTypes.number.isRequired,
+  start: PropTypes.func.isRequired,
+  stop: PropTypes.func.isRequired,
+  reset: PropTypes.func.isRequired,
+  wait: PropTypes.func.isRequired,
+};
